b5c799f493415c6f33234db756e577e308ad13f1
ayman elfadl <aymanelfadl16@gmail.com>
2025-02-22 12:23:19 +0100

    first part

--- Week00/ex03/count.py
+++ Week00/ex03/count.py
@@ -1,7 +1,31 @@
-import string as str
+import string
 
-def text_analyzer(text):
 
+def text_analyzer(text=None):
+
+    if text is None:
+        text = input("What is the text to analyze?\n")
     if isinstance(text, str):
-        i = 0
+        lower = 0
+        upper = 0
+        punctuation = 0
+        spaces = text.count(" ")
+        printable = len(text)
         for c in text:
+            if c.isupper():
+                upper += 1
+            elif c.islower():
+                lower += 1
+            elif c in string.punctuation:
+                punctuation += 1
+        print(f"The text contains {printable} character(s):")
+        print(f"- {upper} upper letter(s)")
+        print(f"- {lower} lower letter(s)")
+        print(f"- {punctuation} punctuation mark(s)")
+        print(f"- {spaces} space(s)")
+    else:
+        print("AssertionError: argument is not a string")
+
+
+if __name__ == "__main__":
+    text_analyzer()
